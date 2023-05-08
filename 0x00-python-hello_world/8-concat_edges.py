e7a2b81f6d66f4de2a035eb31ee7af1895770f11
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-05-08 15:48:44 -0700

    Refactor string concatenation logic in 8-concat_edges.py for improved readability and efficiency

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(" ".join(str.split()[5:7]), end = " ")
-print(" ".join([str.split()[12],str.split()[0]]))
+str = str.split()
+print(" ".join([str[5], str[6], str[12], str[0]]))
