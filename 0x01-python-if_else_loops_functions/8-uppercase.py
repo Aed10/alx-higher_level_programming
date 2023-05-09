60d11fb8ab87e771fb7d2fd6b57b155d93b5ce38
Aed10 <eddyyanyayoub@gmail.com>
2023-05-09 12:19:59 +0100

    Refactor uppercase function to use a simpler implementation using a loop through the string characters and the chr() and ord() functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            if i == len(str) - 1:
-                sp = "\n"
-            else:
-                sp = ""
-            print("{:c}".format(ord(str[i]) - 32), end=sp)
-        else:
-            if i == len(str) - 1:
-                sp = "\n"
-            else:
-                sp = ""
-            print("{:c}".format(ord(str[i])), end=sp)
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
