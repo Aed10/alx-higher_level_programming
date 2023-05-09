0a5ca8242d83be6aa219c08a936cb5a580d241df
Aed10 <eddyyanyayoub@gmail.com>
2023-05-09 12:01:02 +0100

    Refactor uppercase function to print uppercase version of input string with proper spacing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,14 @@
 def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{:c}".format(ord(str[i]) - 32), end="")
-    print()
+            if i == len(str) - 1:
+                sp = "\n"
+            else:
+                sp = ""
+            print("{:c}".format(ord(str[i]) - 32), end=sp)
+        else:
+            if i == len(str) - 1:
+                sp = "\n"
+            else:
+                sp = ""
+            print("{:c}".format(ord(str[i])), end=sp)
