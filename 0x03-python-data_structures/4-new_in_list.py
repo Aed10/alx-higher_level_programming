079f2d1b74f647d59bc2c09e845bdc364c984412
Aed10 <eddyyanyayoub@gmail.com>
2023-05-14 00:14:01 +0100

    Add new function to replace element in list at specific position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that replaces an element in a list at a specific position"""
+
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list[idx] = element
+    return new_list
