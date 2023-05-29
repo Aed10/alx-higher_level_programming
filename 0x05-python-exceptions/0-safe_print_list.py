9b3dadcd27ef8b7eb34afa0a2da04852a1663825
Aed10 <eddyyanyayoub@gmail.com>
2023-05-29 23:50:43 +0100

    Add functionality to count the number of elements printed in safe_print_list() function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
+    nb_elements = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        print()
-        return (i + 1)
+            nb_elements += 1
+
     except BaseException:
-        print()
-        return (i)
+        pass
+    print("")
+    return nb_elements
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
+
+
+main()
