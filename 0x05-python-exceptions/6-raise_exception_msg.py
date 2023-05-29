f1891e3b725c02fff174ab3e8bede211c10e869d
Aed10 <eddyyanyayoub@gmail.com>
2023-05-29 23:35:42 +0100

    Raise NameError with a custom message in raise_exception_msg().

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,14 +1,3 @@
 #!/usr/bin/pyrhon3
 def raise_exception_msg(message=""):
     raise NameError(message)
-
-
-def main():
-    try:
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
-
-
-if __name__ == "__main__":
-    main()
