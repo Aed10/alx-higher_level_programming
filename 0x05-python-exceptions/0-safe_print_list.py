4fd433e9b783f2b5f345adf8dc42afbe4feeb417
Aed10 <eddyyanyayoub@gmail.com>
2023-05-29 22:56:51 +0100

    "Refactor safe_print_list function to simplify loop and improve readability."

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usrbin/python3
 
 def safe_print_list(my_list=[], x=0):
-
-    nb_print = 0
     try:
-        for index in range(0, x):
-            print(my_list[index], end='')
-            nb_print += 1
+        for i in range(x):
+            print(my_list[i], end="")
         print()
-
+        return (i + 1)
     except IndexError:
         print()
-
-    return nb_print
+        return (i)
