b7f4895c836b82490c4ce11b7b4f46150d395d9f
Aed10 <eddyyanyayoub@gmail.com>
2023-05-31 00:13:49 +0100

    Add Square class with size attribute and area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Return the current area of the square"""
+        return self.__size**2
