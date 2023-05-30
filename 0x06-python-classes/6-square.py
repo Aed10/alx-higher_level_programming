14fe909f0f96ccd37270b6839696abda80c0e19e
Aed10 <eddyyanyayoub@gmail.com>
2023-05-31 00:37:23 +0100

    Refactor Square class with size and position attributes and related methods

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,46 +1,23 @@
 #!/usr/bin/python3
-"""Defines a square"""
+"""
+square class
+"""
 
 
 class Square:
-    """Represents a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
 
-    def __init__(self, size=0):
-        """Initialize a new Square"""
-        self.__size = size
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-    def area(self):
-        """Return the current area of the square"""
-        return self.__size**2
+    """
+    creates a square object
+    """
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
         return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Set the size of the square."""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >=0")
-        self.__size = value
-
-    def my_print(self):
-        if self.size == 0:
-            print()
-            return
-        for x in range(self.position[1]):
-            print()
-        for x in range(self.size):
-            print("{}{}".format(" " * self.position[0], "#" * self.size))
         """
-        prints a square of hashtags based on position and size
+        gets size
         """
 
     @property
@@ -69,4 +46,35 @@ class Square:
         Raise:
             ValueError
             TypeError
+        """
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+        """
+        sets size
+        size has to be an integer and positive
+        """
+
+    def area(self):
+        return self.__size**2
+        """
+        returns the area of the size of the square
+        """
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+        for x in range(self.position[1]):
+            print()
+        for x in range(self.size):
+            print("{}{}".format(" " * self.position[0], "#" * self.size))
+        """
+        prints a square of hashtags based on position and size
         """
