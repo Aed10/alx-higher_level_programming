a66a418dce7c735597baf157c0a07cfbaa6560b4
Aed10 <eddyyanyayoub@gmail.com>
2023-05-31 00:31:36 +0100

    Refactor Square class to include getter and setter methods for position attribute in a concise and clear way

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,18 +41,30 @@ class Square:
                     print("#", end="")
                 print()
 
-        @property
-        def position(self):
-            """Get/set the current position of the square."""
-            return self.__position
+    @property
+    def position(self):
+        return self.__position
+        """
+        gets position
+        """
 
-        @position.setter
-        def position(self, value):
-            """Set the position of the square."""
-            if type(value) is not tuple or len(value) != 2:
-                raise TypeError("position must be a tuple of 2 positive integers")
-            if type(value[0]) is not int or value[0] < 0:
-                raise TypeError("position must be a tuple of 2 positive integers")
-            if type(value[1]) is not int or value[1] < 0:
-                raise TypeError("position must be a tuple of 2 positive integers")
-            self.__position = value
+    @position.setter
+    def position(self, value):
+        if (
+            type(value) is not tuple
+            or len(value) is not 2
+            or type(value[0]) is not int
+            or type(value[1]) is not int
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+        """
+        sets position
+        position has to be a tuple of positive integers
+        Raise:
+            ValueError
+            TypeError
+        """
