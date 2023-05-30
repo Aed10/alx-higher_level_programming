8d56081d25e8e485ccde7781cd27f0430ed75f69
Aed10 <eddyyanyayoub@gmail.com>
2023-05-31 00:02:40 +0100

    Add author and docstring to Square class definition in 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# 0-square.py ED-Ayoub
+"""Defines a square """
+
+
 class Square:
+    """Does nothing"""
+
     pass
-    """
-    Empty class Square that defines a square
-    """
