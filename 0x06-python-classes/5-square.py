a4e36c963f9c6b4ef741488fbe442697d7c96289
Aed10 <eddyyanyayoub@gmail.com>
2023-05-31 00:19:29 +0100

    Add Square class with methods for area, size, and printing

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Return the current area of the square"""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
