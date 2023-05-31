8e9f247ca47c3615b3a7cd24002d8371037b51b6
Aed10 <eddyyanyayoub@gmail.com>
2023-06-01 00:17:39 +0100

    Add VSCode settings and update Square class method to handle position in print statement

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,30 +54,19 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             for j in range(self.__size):
-                print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+                print(
+                    "{}{}".format(" " * self.__position[0], "#" * self.__size)
+                )
             return "{}{}".format(" " * self.__position[0], "#" * self.__size)
 
 
 def main():
     """main"""
-    my_square_1 = Square(3)
-    my_square_1.my_print()
+    my_square = Square(3, (1, 1))
+    my_square.my_print()
+    my_square = Square(5, (3, 2))
+    my_square.my_print()
 
-    print("--")
-
-    my_square_2 = Square(3, (1, 1))
-    my_square_2.my_print()
-
-    print("--")
-
-    my_square_3 = Square(3, (3, 0))
-    my_square_3.my_print()
-
-    print("--")
-
-
-if __name__ == "__main__":
-    main()
 
 if __name__ == "__main__":
     main()
