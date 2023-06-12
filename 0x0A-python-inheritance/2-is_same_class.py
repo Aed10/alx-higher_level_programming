90a5baa787f9f9fadb4f61b5ce203334c27cd869
Aed10 <eddyyanyayoub@gmail.com>
2023-06-12 20:09:15 +0100

    Add docstring to is_same_class function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,13 @@
 
 
 def is_same_class(obj, a_class):
+    """Function that returns True if the object is exactly an instance of the
+        specified class ; otherwise False
+    Args:
+        obj: object to check
+        a_class: class to check
+    Returns:
+        True if the object is exactly an instance of the specified class ;
+        otherwise False
+    """
     return type(obj) == a_class
