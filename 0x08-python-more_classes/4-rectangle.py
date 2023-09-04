9a2c7d0923ffc5a3b38cc6cb953b399dab772c03
Aed10 <eddyyanyayoub@gmail.com>
2023-09-04 22:30:53 +0100

    Fix typo in the string representation of the rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -95,4 +95,4 @@ class Rectangle:
         """return a string representation of the rectangle to be able
         to recreate a new instance by using eval()
         """
-        return f"Rectangle({self.__width},{self.__height})"
+        return f"Rectangle({self.__width}), {self.__height})"
