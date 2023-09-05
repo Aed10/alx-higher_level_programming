ee40acbe8bd490886d8baa3de0fe4d96fbb0f083
Aed10 <eddyyanyayoub@gmail.com>
2023-09-05 21:49:59 +0100

    Refactor copy_list function to remove unnecessary comments and fix variable names

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,16 +1,3 @@
-#!/usr/bin/python3
-
-""" Copy a list object. """
-
-
 def copy_list(orig_list):
-    """Copy a list.
-
-    Args:
-        l(list): list to be copied.
-
-    Return:
-        A copy of list l
-    """
     l_cpy = orig_list.copy()
     return l_cpy
