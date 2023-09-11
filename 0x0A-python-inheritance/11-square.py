09f4fdd73c49f97221d663f5dd3f99565b4c3558
Aed10 <eddyyanyayoub@gmail.com>
2023-09-12 00:10:33 +0100

    Add Square class that inherits from Rectangle and implements methods for calculating area and string representation of a square in the 0x0A-python-inheritance directory

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Module that contains a class Square that inherits from Rectangle
+(9-rectangle.py).
+(task based on 10-square.py)"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that defines a Square (inherits from Rectangle (9-rectangle.py).
+    (task based on 10-square.py)"""
+
+    def __init__(self, size):
+        """Initializer function for a Square.
+        Args:
+            size: size of the Square
+        Attributes:
+            __size: size of the Square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Method that returns the area of the Square"""
+        return self.__size**2
+
+    def __str__(self):
+        """Method that returns a string representation of the Square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
