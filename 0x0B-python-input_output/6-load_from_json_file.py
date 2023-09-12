303ea4ca41ec6f2478a51d5c0879d9639edf931d
Aed10 <eddyyanyayoub@gmail.com>
2023-09-13 00:38:09 +0100

    Refactor load_from_json_file function to use json.load() instead of json.loads() and update shebang line

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,19 +1,10 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """Module that creates an Object from a “JSON file” """
+
 import json
 
 
 def load_from_json_file(filename):
-    """
-    Function that creates an Object from a JSON file.
-
-
-    Args:
-        @filename: A JSON file.
-
-    Return:
-        A Python Object.
-    """
-    with open(filename, "r", encoding="utf-8") as json_file:
-        return json.loads(json_file)
-
+    """Creates an Object from a “JSON file”"""
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
