e251801881a9ab8c4d3d3b18ac96d1350c016def
Aed10 <eddyyanyayoub@gmail.com>
2023-12-13 18:05:49 +0100

    Add SQL queries to list all privileges for users user_0d_1 and user_0d_2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
--- Contenant: 0-privileges.sql
-SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
