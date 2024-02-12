58b8c16f4b44b94a418818fa8f9453a1a26ec8e7
Aed10 <eddyyanyayoub@gmail.com>
2024-02-12 23:42:28 +0100

    Update argument output messages in JavaScript file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // print process.argv
 if (process.argv.length === 2) {
-  console.log('No arguments');
+    console.log('No argument');
 } else {
-  console.log('Arguments found');
+    console.log('Arguments found');
 }
