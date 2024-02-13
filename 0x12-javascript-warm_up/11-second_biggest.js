c8a9f3acbac6540d0672e1a898c86c43ff5ac86f
Aed10 <eddyyanyayoub@gmail.com>
2024-02-13 02:13:25 +0100

    Add logic to find the second biggest number in a list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
 let max = 0;
+const list = [];
+if (process.argv.length === 2) {
+  max = 0;
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg = Number(process.argv[i]);
 
-for (let i = 2; i < process.argv.length; i++) {
-  const arg = Number(process.argv[i]);
-
-  if (arg > max) {
-    max = arg;
+    if (arg > max) {
+      max = arg;
+      list.push(max);
+    }
   }
+  max = list.slice(-2, -1)[0];
 }
 
 console.log(max);
