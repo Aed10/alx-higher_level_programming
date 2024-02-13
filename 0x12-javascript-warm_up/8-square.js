8c88e41c0a538febf8ec198eed2918792f527e95
Aed10 <eddyyanyayoub@gmail.com>
2024-02-13 01:28:20 +0100

    Fix indentation and add missing spaces in 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    if (Number.isFinite(Number(process.argv[2]))) {
-        const numberOfOccurrences = Math.round(Number(process.argv[2]));
+  if (Number.isFinite(Number(process.argv[2]))) {
+    const numberOfOccurrences = Math.round(Number(process.argv[2]));
 
-        for (let i = 0; i < numberOfOccurrences; i++) {
-            let row = '';
-            for (let j = 0; j < numberOfOccurrences; j++) { row += 'X'; }
-            console.log(row);
-        }
-    } else {
-        console.log('Missing size');
+    for (let i = 0; i < numberOfOccurrences; i++) {
+      let row = '';
+      for (let j = 0; j < numberOfOccurrences; j++) { row += 'X'; }
+      console.log(row);
     }
+  } else {
+    console.log('Missing size');
+  }
 }
