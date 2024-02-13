2b74344f9f3f93e12ff0482b77d6105384c77581
Aed10 <eddyyanyayoub@gmail.com>
2024-02-13 01:25:18 +0100

    Fix error message for missing size in square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   if (Number.isFinite(Number(process.argv[2]))) {
     const numberOfOccurrences = Math.round(Number(process.argv[2]));
+
     for (let i = 0; i < numberOfOccurrences; i++) {
       let row = '';
       for (let j = 0; j < numberOfOccurrences; j++) { row += '#'; }
       console.log(row);
     }
+  } else {
+    console.log('Missing size');
   }
 }
