adadde775a9328ee26db36a1e7d40f315572e23d
Aed10 <eddyyanyayoub@gmail.com>
2024-02-16 22:54:30 +0100

    Add script to list all states from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/home/alpha_10/venv/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa:
+
+ - Your script should take 3 arguments: mysql username, mysql password, and database name (no argument validation needed)
+ - You must use the module MySQLdb (import MySQLdb)
+ - Your script should connect to a MySQL server running on localhost at port 3306
+ - Results must be sorted in ascending order by states.id
+ - Results must be displayed as they are in the example below
+ - Your code should not be executed when imported"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    Connectivity = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    Cursor = Connectivity.cursor()
+    Cursor.execute("SELECT * FROM `states`;")
+    [print(state) for state in Cursor.fetchall()]
+    Cursor.close()
+    Connectivity.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# 0x0F-python-object_relational_mapping
+
+This project aims to create a Python script that can perform object-relational mapping (ORM) with a MySQL database. The script can connect to the database, execute SQL queries, and map Python classes to MySQL tables. The project uses the MySQLdb module and the SQLAlchemy ORM.
