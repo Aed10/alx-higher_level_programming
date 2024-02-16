27bd2f22f95f2bc6aa35b07bfc7455ed5050bb4b
Aed10 <eddyyanyayoub@gmail.com>
2024-02-16 23:46:23 +0100

    Refactor code to filter and print only states starting with "N".

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,11 +21,8 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
     )
-    query = "SELECT id, name FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC;"
+
     Cursor = Connectivity.cursor()
-    Cursor.execute(query)
-    [print(state) for state in Cursor.fetchall()]
-    Cursor.close()
-    Connectivity.close()
+    Cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in Cursor.fetchall() if state[1][0] == "N"]
