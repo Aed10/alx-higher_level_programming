9145e7be8e2a1e36601afb1f087a2faa28a3ddd6
Aed10 <eddyyanyayoub@gmail.com>
2024-02-18 14:34:33 +0100

    Add function description for listing states in fetch_all script

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database and prints a list of cities whose
+belongs to a given @state_name as argument.
+Safe from SQL injection
+
+Usage: python3 mysql_example.py <username> <password> <database> <state_name>
+
+Dependencies:
+    - MySQL-python library
+
+Example:
+    python3 mysql_example.py username password database 'Arizona'
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,6 +20,12 @@ from model_state import Base, State
 
 
 def list_states(username, password, database_name):
+    """Prints a list of states whose names matches @name argument.
+
+    Args:
+        username (str): The MySQL username.
+        password (str): The MySQL password.
+        database_name (str): The MySQL database name."""
     # Create engine
     engine = create_engine(
         f"mysql://{username}:{password}@localhost:3306/{database_name}"
