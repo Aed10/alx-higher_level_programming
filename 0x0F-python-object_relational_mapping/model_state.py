e141533da2bed5df4f7c23b836d9cab70537b914
Aed10 <eddyyanyayoub@gmail.com>
2024-02-18 14:16:37 +0100

    Update State model documentation string and attributes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
 
+Args:
+    None
+
+Returns:
+    None
+
+Raises:
+    None
+
+Attributes:
+    Base (sqlalchemy.ext.declarative.declarative_base):
+    The SQLAlchemy base class.
+    Column (sqlalchemy.Column): The SQLAlchemy column class.
+    Integer (sqlalchemy.Integer): The SQLAlchemy integer column type.
+    String (sqlalchemy.String): The SQLAlchemy string column type.
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
