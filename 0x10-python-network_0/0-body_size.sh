c8499e74632445bf5e729682190020e80d687d72
Aed10 <eddyyanyayoub@gmail.com>
2024-02-29 23:20:19 +0100

    Update script to print body size directly from URL parameter

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script will print the size of the body of the response.
-URL=$1
-curl -sI "$URL" | grep -i Content-length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-length | awk '{print $2}'
