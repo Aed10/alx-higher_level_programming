92940840f88f7f647b987bb51a6da446e3cf0190
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 00:43:42 +0100

    Update command to use cut instead of awk for better output

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will display all HTTP methods the server will accept.
-curl -X OPTIONS -sI "$1" | grep -i Allow | awk '{print $2, $3, $4}' | sed 's/,$//'
+curl -X OPTIONS -sI "$1" | grep -i Allow | cut -d ' ' -f2-
