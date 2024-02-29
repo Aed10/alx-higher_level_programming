6f18d8b146763711875c7be9894ce303d411ff16
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 00:41:56 +0100

    Update curl command to include response headers in script

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will display all HTTP methods the server will accept.
-curl -X OPTIONS -s "$1" | grep -i Allow | awk '{print $2, $3, $4}'
+curl -X OPTIONS -sI "$1" | grep -i Allow | awk '{print $2, $3, $4}' | sed 's/,$//'
