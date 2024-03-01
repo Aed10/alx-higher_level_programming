006b2d78db8cf64e2835169d30814395e328a2f3
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 20:49:51 +0100

    Update print format for body response output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 url = "https://alx-intranet.hbtn.io/status"
 r = requests.get(url)
 print("Body response:")
-print(f"\t - type: {type(r.text)}")
+print(f"\t- type: {type(r.text)}")
 print(f"\t- content: {r.text}")
