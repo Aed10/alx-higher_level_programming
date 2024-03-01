5a6b0570cfeeede182d940379fae9f995918f7d7
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 22:07:11 +0100

    Update error handling and response check in 7-error_code script

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,11 +6,8 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception if error occurred
-    except requests.exceptions.HTTPError as err:
-        err_code = err.response.status_code
-        if err_code >= 400:
-            print(f"Error code: {err_code}")
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
