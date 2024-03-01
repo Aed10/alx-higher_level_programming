6f2860abc3ea1cd9ce477563fbee2189c1311593
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 18:46:06 +0100

    Add script to fetch url response and decode utf-8 content

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(req) as response:
+    body = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+# 0x11-python-network_1
+
+This project explores essential Python networking concepts.  You'll delve into using libraries like urllib and requests to interact with web resources and APIs.
