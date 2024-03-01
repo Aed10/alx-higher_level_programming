20842592ad3a3ca78a30430806227d0aaebcb10a
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 01:00:07 +0100

    Update POST request data format to use equal sign

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will send a POST request to a URL passed as an argument, and display the body of the response.
-curl -sX POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" -s "$1"
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -s "$1"
