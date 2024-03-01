bf95eb8194ae18459f1c51977cf8719afcc327bb
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 22:50:36 +0100

    Add script to display GitHub user id using API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+     a Python script that takes your GitHub credentials (username and password)
+     and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+url = "https://api.github.com/user"
+
+response = requests.get(url, auth=(user, passwd))
+
+user_data = response.json()
+user_id = user_data["id"]
+print(f"{user_id}")
