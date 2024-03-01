06dac69dae79cfc72eb7793bad23e250c94759a5
Aed10 <eddyyanyayoub@gmail.com>
2024-03-01 23:27:44 +0100

    Add script to retrieve latest 10 commits from GitHub repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+"""
+This script retrieves the latest 10 commits from a GitHub repository.
+
+Usage:
+    python3 100-github_commits.py <owner> <repo>
+
+Arguments:
+    <owner>: The owner of the GitHub repository.
+    <repo>: The name of the GitHub repository.
+
+Example:
+    python3 100-github_commits.py octocat Hello-World
+
+"""
+
+if __name__ == "__main__":
+    OWNER = sys.argv[1]
+    REPO = sys.argv[2]
+    url = f"https://api.github.com/repos/{OWNER}/{REPO}/commits"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        commit_list = response.json()[:10]
+        for commit in commit_list:
+            sha = commit["sha"]
+            print(f"{sha}: {commit['commit']['author']['name']}")
+    else:
+        print("Error: Failed to retrieve commits")
