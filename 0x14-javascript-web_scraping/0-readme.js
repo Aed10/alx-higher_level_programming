9acabb5e320b39b03262a46a65bf17f04dba95c7
Aed10 <eddyyanyayoub@gmail.com>
2024-05-02 01:17:50 +0100

    Add script to read a file and update README

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Read a  file
+
+const fs = require('fs');
+
+// Read the file
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
